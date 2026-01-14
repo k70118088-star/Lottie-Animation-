24c45c74166cab2111904489412486a5335596f7
k70118088-star <k70118088@gmail.com>
2026-01-14 10:19:33 +0530

    lottie animations

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-const button = document.getElementById("btn");
+const btn = document.getElementById("btn");
 const loader = document.getElementById("loader");
+const success = document.getElementById("success");
 
-button.addEventListener("click", async () => {
-  button.classList.add("hidden");
-  loader.classList.remove("hidden");
+const SUCCESS_DURATION = 1600;
 
-  await customElements.whenDefined("dotlottie-wc");
-  loader.play();
+btn.addEventListener("click", () => {
+    
+    btn.classList.add("hidden");
+    loader.classList.remove("hidden");
 
+    
+    setTimeout(() => {
+        loader.classList.add("hidden");
+        success.classList.remove("hidden");
+
+        
+        setTimeout(() => {
+            success.classList.add("hidden");
+            btn.classList.remove("hidden");
+        }, SUCCESS_DURATION);
+    }, 3500); 
 });
+
